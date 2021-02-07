64993e99b490c9077c5ea9e63ece6bb602ec1a7e
CJ <48690692+cjramseyer@users.noreply.github.com>
2021-02-07 13:10:15 -0500

    Delete node_modules directory

--- node_modules/caldate/lib/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var M = module.exports = require('./CalDate');
-M.pad0 = require('./utils').pad0;
-M.toNumber = require('./utils').toNumber;
-M.isDate = require('./utils').isDate;
-M.toYear = require('./utils').toYear;
